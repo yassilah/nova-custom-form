2acfe8e563e696fae55236f8acdd9594005894a8
yassipad <yasser.lahbibi@hotmail.com>
2018-09-24 12:14:42 +0200

    type collection for attachcustomforms

--- src/NovaCustomFormServiceProvider.php
+++ src/NovaCustomFormServiceProvider.php
@@ -5,6 +5,7 @@ namespace Yassi\NovaCustomForm;
 use Laravel\Nova\Nova;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Nova\Events\ServingNova;
+use Illuminate\Support\Collection;
 
 class NovaCustomFormServiceProvider extends ServiceProvider
 {
@@ -27,7 +28,7 @@ class NovaCustomFormServiceProvider extends ServiceProvider
      * This method filters the resources that
      * user NovaCustomForm. 
      * 
-     * @return array
+     * @return Collection
      */
     private function filterResources () {
         return collect(Nova::availableResources(request()))->filter(function ($resource) {
@@ -39,9 +40,10 @@ class NovaCustomFormServiceProvider extends ServiceProvider
      * This method maps the available custom forms
      * and keys them with their corresponding resource.
      * 
-     * @return array
+     * @param Collection $resources
+     * @return Collection
      */
-    private function attachCustomForms (array $resources) {
+    private function attachCustomForms (Collection $resources) {
         return $resources->mapWithKeys(function ($resource) {
             return [ 
                 (new $resource::$model)->getTable() => [
