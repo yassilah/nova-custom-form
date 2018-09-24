6877ef62cb173a109decaed2428a2974e1da0108
yassipad <yasser.lahbibi@hotmail.com>
2018-09-24 11:51:46 +0200

    filterResources && atatchCustomForms

--- src/NovaCustomFormServiceProvider.php
+++ src/NovaCustomFormServiceProvider.php
@@ -17,21 +17,39 @@ class NovaCustomFormServiceProvider extends ServiceProvider
     {
         Nova::serving(function (ServingNova $event) {
             Nova::provideToScript([
-                'customForms' => collect(Nova::availableResources(request()))
-                ->filter(function ($resource) {
-                    return $resource::form(request());
-                })
-                ->mapWithKeys(function ($resource) {
-                    return [ 
-                        (new $resource::$model)->getTable() => [
-                            'create' => $resource::form(request())->getCreateComponent(),
-                            'edit' => $resource::form(request())->getUpdateComponent()
-                        ]
-                    ];
-                })
+                'customForms' => $this->attachCustomForms($this->filterResources())
             ]);
             Nova::script('nova-custom-forms', __DIR__ . '/../dist/js/nova-custom-forms.js');
         });
+    }
+
+    /**
+     * This method filters the resources that
+     * user NovaCustomForm. 
+     * 
+     * @return array
+     */
+    private function filterResources () {
+        return collect(Nova::availableResources(request()))->filter(function ($resource) {
+            return method_exists($resource, 'form') ? $resource::form(request()) : false;
+        });
+    }
+
+    /**
+     * This method maps the available custom forms
+     * and keys them with their corresponding resource.
+     * 
+     * @return array
+     */
+    private function attachCustomForms (array $resources) {
+        return $resources->mapWithKeys(function ($resource) {
+            return [ 
+                (new $resource::$model)->getTable() => [
+                    'create' => $resource::form(request())->getCreateComponent(),
+                    'edit' => $resource::form(request())->getUpdateComponent()
+                ]
+            ];
+        })
     }
 
     /**
